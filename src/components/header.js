447292a33cd9771424b927baa1e2952bc235a97a
Samuel Brehm <samuelbrehm@gmail.com>
2020-02-21 16:15:52 -0300

    Builded cards and styling header menu

--- src/components/header.js
+++ src/components/header.js
@@ -5,20 +5,46 @@ import logo from '../images/logo-designcode.svg'
 
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/">
-        <img src={logo} width="30" />
-      </Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/doenloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy">
-        <button>Buy</button>
-      </Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else this.setState({ hasScrolled: false })
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}
+      >
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img src={logo} width="30" />
+          </Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/doenloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy">
+            <button>Buy</button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
