02dda7fa06a62367b5fd53db70073e1103c2ee55
Samuel Brehm <samuelbrehm@gmail.com>
2020-02-24 17:59:04 -0300

    Added contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: 'Design Code 3',
+    title: 'Design+Code 3',
+    description:
+      'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords:
+      'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: '8ngjixkqzn2o',
+        accessToken: 'jgQgHaVzstjXss2jIdRTq9udO8s-HSui8GtW5KlX_L8',
+      },
+    },
+  ],
 }
